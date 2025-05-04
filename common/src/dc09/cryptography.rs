b774146b88947645f3c10d298008e3eaaeed6e22
Piotr Frankowski <piotr.frankowski@vp.pl>
2025-05-04 13:04:57 +0200

    Add DC09 message tests

--- common/src/dc09/cryptography.rs
+++ common/src/dc09/cryptography.rs
@@ -1,17 +1,15 @@
 use aes::cipher::{BlockDecryptMut, BlockEncryptMut, KeyIvInit, block_padding::NoPadding};
 use rand::{Rng, distr::Alphanumeric};
 
-const ZEROS_IV_16: [u8; 16] = [0u8; 16];
-const ZEROS_IV_24: [u8; 24] = [0u8; 24];
-const ZEROS_IV_32: [u8; 32] = [0u8; 32];
+const ZEROS_IV: [u8; 16] = [0u8; 16];
 
 /// Encrypts the DC09 message using AES CBC.  
 /// **Note** that it adds padding if necessary.
 pub fn encrypt(message: &str, key: &[u8]) -> Option<String> {
     match key.len() {
-        16 => encrypt_internal::<cbc::Encryptor<aes::Aes128>>(message, key, &ZEROS_IV_16),
-        24 => encrypt_internal::<cbc::Encryptor<aes::Aes192>>(message, key, &ZEROS_IV_24),
-        32 => encrypt_internal::<cbc::Encryptor<aes::Aes256>>(message, key, &ZEROS_IV_32),
+        16 => encrypt_internal::<cbc::Encryptor<aes::Aes128>>(message, key, &ZEROS_IV),
+        24 => encrypt_internal::<cbc::Encryptor<aes::Aes192>>(message, key, &ZEROS_IV),
+        32 => encrypt_internal::<cbc::Encryptor<aes::Aes256>>(message, key, &ZEROS_IV),
         _ => None,
     }
 }
@@ -21,9 +19,9 @@ pub fn encrypt(message: &str, key: &[u8]) -> Option<String> {
 pub fn decrypt(message: &str, key: &[u8]) -> Option<String> {
     let message = hex::decode(message).ok()?;
     match key.len() {
-        16 => decrypt_internal::<cbc::Decryptor<aes::Aes128>>(message, key, &ZEROS_IV_16),
-        24 => decrypt_internal::<cbc::Decryptor<aes::Aes192>>(message, key, &ZEROS_IV_24),
-        32 => decrypt_internal::<cbc::Decryptor<aes::Aes256>>(message, key, &ZEROS_IV_32),
+        16 => decrypt_internal::<cbc::Decryptor<aes::Aes128>>(message, key, &ZEROS_IV),
+        24 => decrypt_internal::<cbc::Decryptor<aes::Aes192>>(message, key, &ZEROS_IV),
+        32 => decrypt_internal::<cbc::Decryptor<aes::Aes256>>(message, key, &ZEROS_IV),
         _ => None,
     }
 }
@@ -32,7 +30,7 @@ fn encrypt_internal<C>(message: &str, key: &[u8], iv: &[u8]) -> Option<String>
 where
     C: BlockEncryptMut + KeyIvInit,
 {
-    let mut padded = pad_message(message, key.len()).into_bytes();
+    let mut padded = pad_message(message).into_bytes();
     let message_len = padded.len();
     let cipher = C::new_from_slices(key, iv).ok()?;
     cipher.encrypt_padded_mut::<NoPadding>(&mut padded, message_len).ok()?;
@@ -49,8 +47,8 @@ where
 }
 
 /// Adds padding to the start of a DC09 message.
-fn pad_message(message: &str, key_len: usize) -> String {
-    let pad = key_len.saturating_sub(message.len() % key_len);
+fn pad_message(message: &str) -> String {
+    let pad = 16usize.saturating_sub(message.len() % 16usize);
     let mut rng = rand::rng();
     let mut result = String::with_capacity(message.len() + pad);
     for _ in 0..pad {
